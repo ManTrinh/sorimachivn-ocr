357eab454df1baf74d7322aa9a5c4f8b79983851
ManTrinh <trinhthongman96@gmail.com>
2023-10-10 10:47:13 +0700

    change

--- main.py
+++ main.py
@@ -39,4 +39,4 @@ def index():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run()
+    app.run(debug=False)
