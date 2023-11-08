1f103a47e6f54e69ffcbe26026e073844854bddf
ManTrinh <trinhthongman96@gmail.com>
2023-11-08 15:53:05 +0700

    Detect Date

--- convert_YMD.py
+++ convert_YMD.py
@@ -14,11 +14,11 @@ import datetime
 
 # 各年号の元年を定義
 eraDict = {
-    "明治": [1868,908,45],
-    "大正": [1912,712,15],
-    "昭和": [1926,1225,64],
-    "平成": [1989,108,31],
-    "令和": [2019,501],
+    "明治": ["M",1868,908,45],
+    "大正": ["T",1912,712,15],
+    "昭和": ["S",1926,1225,64],
+    "平成": ["H",1989,108,31],
+    "令和": ["R",2019,501],
 }
 
 formatted = {}
@@ -27,9 +27,9 @@ def get_gengo_year(year,md):
     gengo = ""
     reversed_era_items = reversed(list(eraDict.items()))
     for era, era_data in reversed_era_items:
-        tmp_val = (era_data[0] * 10000 + era_data[1])
-        if year <= era_data[2]:
-            ymd_val = (era_data[0] + year - 1) * 10000 + md
+        tmp_val = (era_data[1] * 10000 + era_data[2])
+        if year <= era_data[3]:
+            ymd_val = (era_data[1] + year - 1) * 10000 + md
             if ymd_val >= tmp_val: 
                 gengo = era
                 break
@@ -41,14 +41,18 @@ def japanese_calendar_converter(text):
     day_ = int(re.sub(r'\D', '', text.split("月")[1].split("日")[0]))
     md_val = month_*100 + day_
     # Tách năm, tháng và ngày từ chuỗi
-    if len(text.split("年")[0]) == 2:
+    if len(text.split("年")[0]) > 0 and len(text.split("年")[0]) < 3:
         # Lấy năm hiện tại
         current_year = datetime.datetime.now().year
+        compare_year = int("{}".format(current_year)[-2:])
         year_ = int(text.split("年")[0])
         for era, era_data in eraDict.items():
             if era == "令和":
-                era_data.append(current_year-era_data[0]+1)
-        text = "{}{}".format(get_gengo_year(year_, md_val),text)
+                era_data.append(current_year-era_data[1]+1)
+        if year_ <= compare_year:
+            return datetime.date(2000 + year_, month_, day_)
+        else:
+            text = "{}{}".format(get_gengo_year(year_, md_val),text)
         
     # 正規化
     normalized_text = unicodedata.normalize("NFKC", text)
@@ -67,7 +71,7 @@ def japanese_calendar_converter(text):
             if date.group("year") == "元":
                 year = eraDict[era]
             else:
-                year = eraDict[era][0] + int(date.group("year")) - 1
+                year = eraDict[era][1] + int(date.group("year")) - 1
     
     # date型に変換して返す
     return datetime.date(year, int(date.group("month")), int(date.group("day")))
@@ -79,6 +83,14 @@ def conv_str_to_date(text):
     day_ = int(re.sub(r'\D', '', text.split("月")[1].split("日")[0]))
     return datetime.date(year_, month_, day_)
 
+def conv_gengo_char(val):
+    gengo_year = 0
+    count_year = int("".join(re.findall(r'\d', val)))
+    for era, era_data in eraDict.items():
+        if era_data[0] in val:
+            gengo_year = era_data[1] + count_year - 1
+            break
+    return gengo_year
 
 def json_date_result(text):
     if len(text) == 0:
@@ -96,9 +108,12 @@ def json_date_result(text):
     else:
         arr_format = []
         arr_format = text.split("/")
+        gengo_year = ""
         if len(arr_format) > 0:
+            gengo_year = conv_gengo_char(arr_format[0])
+            if gengo_year > 0:
+                arr_format[0] = gengo_year
             formatted["day"] = "{}".format(f"{arr_format[2]:02}")      
             formatted["month"] = "{}".format(f"{arr_format[1]:02}")  
             formatted["year"] = "{}".format(arr_format[0])
-    return formatted            
-
+    return formatted
